e6b43be9ef1e901a5462539345f226b8002b4325
Shawn Beelman <shawn@sbgraphicdesign.com>
2016-03-11 10:57:03 -0600

    updated version number

--- simple-recipe.php
+++ simple-recipe.php
@@ -4,7 +4,7 @@ Plugin Name: Simple Recipe
 Plugin URI: https://github.com/Protohominid/simple-recipe
 Description: Creates the "Recipe" post type and shortcode to insert into posts.
 Author: Shawn Beelman
-Version: 0.6.2
+Version: 0.7.0
 Author URI: http://www.sbgraphicdesign.com
 License: GPLv2
 Text Domain: simple-recipe
